40a6a9941b56d939ff69d764e8891dd45b44c07b
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-03-01 12:02:22 +0200

    kiss update

--- Task 8/script.js
+++ Task 8/script.js
@@ -10,23 +10,15 @@ function Calculator(num1, num2) {
 	this.num1 = num1;
 	this.num2 = num2;
 
-	this.sum = (num1, num2) => {
-		return num1 + num2;
-	};
-	this.subtraction = (num1, num2) => {
-		return num1 - num2;
-	};
-	this.multiplication = (num1, num2) => {
-		return num1 * num2;
-	};
-	this.division = (num1, num2) => {
-		return num1 / num2;
-	};
+	this.sum = (num1, num2) => num1 + num2;
+	this.subtraction = (num1, num2) => num1 - num2;
+	this.multiplication = (num1, num2) => num1 * num2;
+	this.division = (num1, num2) => num1 / num2;
 }
 
 const numbers = new Calculator();
 
 console.log(numbers.sum(2, 2));
-console.log(numbers.subtraction(2, 2));
-console.log(numbers.multiplication(2, 2));
-console.log(numbers.division(2, 2));
+console.log(numbers.subtraction(6, 3));
+console.log(numbers.multiplication(10, 2));
+console.log(numbers.division(20, 5));
